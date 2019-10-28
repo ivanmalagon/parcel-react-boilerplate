25fbc03fd4352bd7fa64bc6dfb30ceb1cee8abd5
ivanmalagon <hacheka@gmail.com>
2019-10-28 10:15:57 +0100

    add react and styled components

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.h1`
+  color: tomato;
+`;
+
+const App = () => <AppWrapper>This is an app</AppWrapper>;
+
+export default App;
